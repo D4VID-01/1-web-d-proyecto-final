7adc12b04675b71cde2a99097b41f7745953dff1
luisDavid <ld0110mp@gmail.com>
2025-08-30 23:28:34 -0600

    feat: Agrega model alumnos y routes

---
+++ 3-full-stack/backend/src/models/alumno.model.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+
+const alumnoSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    edad: {
+        type: Number,
+        require: true,
+        trim: true
+    }, grupo:{
+        type: String,
+        require: true,
+        trim: true
+    }
+}, {
+    timestamps: true
+});
+
+export default mongoose.model('Alumno', AlumnoSchema);
---
+++ 3-full-stack/backend/src/routes/alumnos.routes.js
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import alumnoModel from "../models/alumno.model.js";
+
+const router = Router();
+
+// Crear alumno
+router.post('/', async (req, res) => {
+    const nuevoAlumno = new alumnoModel(req.body);
+    await nuevoAlumno.save();
+    res.json(nuevoAlumno);
+})
+
+// Obtener todos los alumnos
+router.get('/', async (req, res) => {
+    const alumnos = await alumnoModel.find();
+    res.json(alumnos);
+});
+
+// Modificar alumno
+router.put('/:id', async (req, res) => {
+    const alumnoActualizado = await alumnoModel.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    res.json(alumnoActualizado);
+});
+
+// Eliminar alumno
+router.delete('/', async (req, res) => {
+    await alumnoModel.findByIdDelete(req.params.id);
+    res.json({mensaje: "Alumno eliminado"});
+});
+
+export default router;
+
