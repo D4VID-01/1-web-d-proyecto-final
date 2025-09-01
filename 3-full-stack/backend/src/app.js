a5ae56bbb9d83e3fe2fbb9c0481445bf726934e6
luisDavid <ld0110mp@gmail.com>
2025-09-01 13:52:22 -0600

    feat: Agrega route, schema y controller para auth

--- 3-full-stack/backend/src/app.js
+++ 3-full-stack/backend/src/app.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import { config } from 'dotenv'
 import alumnosRouter from './routes/alumnos.routes.js'
+import authRouter from './routes/auth.routes.js'
 
 
 config()
@@ -12,7 +13,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/api/alumnos", alumnosRouter);
+app.use('/api/alumnos', alumnosRouter);
+app.use('/api', authRouter);
 
 
 mongoose.connect(process.env.MONGO_KEY).then( () => console.log('Conectado a Mongo DB') );
