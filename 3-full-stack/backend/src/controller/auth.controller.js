afabb09e3cd3a9ff9fac046bf930ba4686111694
luisDavid <ld0110mp@gmail.com>
2025-09-02 19:44:59 -0600

    feat(auth.contoller): Agrega JsonWebToken en cookie

--- 3-full-stack/backend/src/controller/auth.controller.js
+++ 3-full-stack/backend/src/controller/auth.controller.js
@@ -1,9 +1,10 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
-    
-    const {username, email, password} = req.body;
+
+    const { username, email, password } = req.body;
 
     try {
 
@@ -16,16 +17,30 @@ export const register = async (req, res) => {
         })
 
         const userSaved = await newUser.save();
-        res.json({
-            id: userSaved._id,
-            username: userSaved.username,
-            email: userSaved.email,
-            createdAt: userSaved.createdAt,
-            updatedAt: userSaved.updatedAt
-        });
+
+        jwt.sign({
+            id: userSaved.id
+        },
+            process.env.SECRET_KEY,
+            {
+                expiresIn: "1d"
+            },
+            (err, token) => {
+                if (err) console.log(err);
+                res.cookie("token", token);
+
+                res.json({
+                    id: userSaved._id,
+                    username: userSaved.username,
+                    email: userSaved.email,
+                    createdAt: userSaved.createdAt,
+                    updatedAt: userSaved.updatedAt
+                });
+            }
+        )
 
     } catch (error) {
-        console.log(error)
+        res.status(500).json({message: error.message})
     }
 }
 
